059d298a27d98967a472cb917d23f1d101848e04
Md. Shihabul Islam <166291764+Shihab23033@users.noreply.github.com>
2025-06-17 01:00:20 +0600

    iu

---
+++ Assignments/shihab_MATH.py
@@ -0,0 +1,16 @@
+import mpmath as mp
+def print_pi_to_n_digits(n):
+
+    # Set precision to n decimal places
+
+    mp.dps = n + 1  # Adding 1 to account for the "3." before the decimals
+
+    # Return pi to n decimal places as a string
+
+    return str(mp.pi)
+
+if __name__ == "__main__":
+    n = int(input("Enter the number of decimal places for pi: "))
+    
+    pi_value = print_pi_to_n_digits(30)
+    print(f"Pi to {n} decimal places: {pi_value}")
